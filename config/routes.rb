d7a081e3f68997d01441f35860d98d9845dfd1ee
Stephania Gambaroff Thomas <stgambar@gambaroff.com>
2013-12-03 15:26:03 -0800

    Create user resource user and user show page (with factories). #1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 PublicVoid::Application.routes.draw do
+  resources :users
   root  'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
