25d1508fe243a122b783f2f9a3ddcd2a750eab11
Stephania Gambaroff Thomas <stgambar@gambaroff.com>
2013-12-02 18:04:24 -0800

    Add layouts and raouts, add signup page. #1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 PublicVoid::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
+  get "users/new"
+  root  'static_pages#home'
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/help',    to: 'static_pages#help',    via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,30 +1,32 @@
 require 'spec_helper'
 
 describe "Static pages" do
-
+  
+  let(:base_title) { "Public Void" }
+  
   describe "Home page" do
 
     it "should have the content 'Public Void'" do
       visit '/static_pages/home'
       expect(page).to have_content('Public Void')
     end
-    
+
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("Public Void | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
-  
+
   describe "Help page" do
 
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
-    
+
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("Public Void | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 end
