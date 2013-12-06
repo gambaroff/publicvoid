021ab9bc8ef564e7c26e204baeb27bceae228f19
Stephania Gambaroff Thomas <stgambar@gambaroff.com>
2013-12-05 16:06:46 -0800

    Add actions to Transfers controller. #8

--- app/controllers/transfers_controller.rb
+++ app/controllers/transfers_controller.rb
@@ -1,12 +1,38 @@
 class TransfersController < ApplicationController
   before_action :signed_in_user, only: [:create, :destroy]
 
+
+  # GET /transfers
+  # GET /transfers.json
   def index
+    @uploads = Upload.all
   end
+ 
 
+  # POST /transfers
+  # POST /transfers.json
   def create
+   @transfer = Transfer.new(transfer_params)
+   
+     respond_to do |format|
+      if @transfer.save
+        format.html { redirect_to @transfer, notice: 'Upload was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @transfer }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @transfer.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
+
+  # DELETE /transfers/1
+  # DELETE /transfers/1.json
   def destroy
+    @transfer.destroy
+    respond_to do |format|
+      format.html { redirect_to transfers_url }
+      format.json { head :no_content }
+    end
   end
 end
