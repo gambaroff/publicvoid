c428738d6177ed8116cd880ca2675a9a4305fbc0
Dave Thomas <david.thomas@verifi.com>
2013-12-08 19:37:11 -0800

    added fileupload to rspec

--- spec/models/transfer_spec.rb
+++ spec/models/transfer_spec.rb
@@ -3,11 +3,14 @@ require 'spec_helper'
 describe Transfer do
 
   let(:user) { FactoryGirl.create(:user) }
-  before { @transfer = user.transfers.build(content: "Lorem_ipsum.mov") }
+  before {
+    file = File.new(Rails.root + 'spec/support/ico.png')  
+    upload = ActionDispatch::Http::UploadedFile.new(:tempfile => file, :filename => File.basename(file))  
+    @transfer = user.transfers.build(transfered_file: upload) 
+  }
 
   subject { @transfer }
 
-  it { should respond_to(:content) }
   it { should respond_to(:user_id) }
   it { should respond_to(:user) }
   its(:user) { should eq user }
