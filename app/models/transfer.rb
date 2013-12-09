53afe3102d8dc8642cf553162d3b4a268cc7c256
Stephania Gambaroff Thomas <stgambar@gambaroff.com>
2013-12-08 20:30:20 -0800

    Modified upload model to not force file resizing

--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -1,6 +1,6 @@
 class Transfer < ActiveRecord::Base
   belongs_to :user
-  has_attached_file :transfered_file, styles: { original: "4000x4000>", large: "1500x1500>", medium: "300x300>", small: "200x200>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :transfered_file, default_url: "/images/:style/missing.png"
   default_scope -> { order('created_at DESC') }
   validates :user_id, presence: true
 end
